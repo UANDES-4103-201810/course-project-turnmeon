1738b2f8fe6f706d0aa82f90ee3e361ed5b407a2
fjbasualdo <fjbasualdo@miuandes.cl>
2018-05-24 13:40:11 -0400

    user details

--- TurnItOn/app/controllers/application_controller.rb
+++ TurnItOn/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	before_action :configure_permited_parameters, if: :devise_controller?
+	protected
+	def configure_permited_parameters
+		devise_parameter_sanitizer.permit(:sign_up){ |a| a.permit(:first_name, :last_name, :birthdate, :email, :password, :remember_me)}
+		devise_parameter_sanitizer.permit(:sign_in){ |a| a.permit(:email, :password, :remember_me)}
+		devise_parameter_sanitizer.permit(:account_update){ |a| a.permit(:first_name, :last_name, :birthdate, :about, :email, :password)}
+	end
+
 end
--- TurnItOn/db/schema.rb
+++ TurnItOn/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180522231508) do
+ActiveRecord::Schema.define(version: 20180524170849) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -58,6 +58,10 @@ ActiveRecord::Schema.define(version: 20180522231508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthdate"
+    t.text "about"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
