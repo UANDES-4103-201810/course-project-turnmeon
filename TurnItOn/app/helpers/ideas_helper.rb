216e3721a2cc5566d33b1cde4907a524984867f5
fjbasualdo <fjbasualdo@miuandes.cl>
2018-05-25 18:07:44 -0400

    seeds completo

--- TurnItOn/app/helpers/ideas_helper.rb
+++ TurnItOn/app/helpers/ideas_helper.rb
@@ -1,10 +1,12 @@
 module IdeasHelper
 
 	def favourite_idea(idea)
-		if current_user.favourite_ideas.map(&:idea).include?(idea)
-			'text-success'
-		else
-			'text-muted'
+		if user_signed_in?
+			if current_user.favourite_ideas.map(&:idea).include?(idea)
+				'text-success'
+			else
+				'text-muted'
+			end
 		end
 	end
 end
